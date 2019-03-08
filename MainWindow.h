d8cfcab963ba2df6c2da973b0a83fd339f9b2316
adamzhang <mingpingzhang@163.com>
2019-03-08 16:02:05 +0800

    first commit

---
+++ MainWindow.h
@@ -0,0 +1,11 @@
+#ifndef __MAINWINDOW__H
+#define __MAINWINDOW__H
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+	public:
+		MainWindow(QWidget* parent = 0);
+		~MainWindow();
+};
+#endif//__MAINWINDOW__H
